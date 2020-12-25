70171f299e2d5e5b4fdd9187600d39345bcb0f01
joshqinbek-github <joshdeveloper22@gmail.com>
2020-12-25 17:35:08 +0500

    ASdasd

---
+++ src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'ng-app';
+
+  alerted(){
+    alert(this.title + "is working")
+  }
+}
