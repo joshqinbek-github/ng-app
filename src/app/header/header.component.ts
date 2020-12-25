8a4564b36f95d39aad6a50c7c82c07417d94e335
joshqinbek-github <joshdeveloper22@gmail.com>
2020-12-25 18:19:51 +0500

    ASdassssss

---
+++ src/app/header/header.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { Observable } from 'rxjs';
+import { map, shareReplay } from 'rxjs/operators';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css']
+})
+export class HeaderComponent {
+
+
+  constructor(private breakpointObserver: BreakpointObserver) {}
+
+}
